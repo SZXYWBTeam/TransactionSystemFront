493b15caa29ce5448f7c7045cf590e511eccf102
haruka <1196756653@qq.com>
2018-11-02 13:46:35 +0800

    修改toolbar

--- script/toolbar.js
+++ script/toolbar.js
@@ -1,8 +1,15 @@
 function Toolbar(selecor, callback){
+    var title = null ;
+    var backButton = null ;
     this_toolbar = this ;
-    $(selecor).load("/TransactionSystem/commons/toolbar.html", function(){
-        this_toolbar.title = $("#toolbar-title") ;
-        this_toolbar.backButton = $("#toolbar-back-button") ;
-        callback(this_toolbar);
-    }) ;
+    toolbarHtml = '<html><head><link href="/TransactionSystem/css/toolbar.css"rel="stylesheet"type="text/css"/></head><body><div class="app-toolbar app-toolbar-container"id="toolbar"><span class="app-toolbar-left-icon app-toolbar-center-vertical"id="toolbar-back-button"><img src="/TransactionSystem/img/backarrow.png"/></span><span class="app-toolbar-title"id="toolbar-title"></span></div></body></html>' ;
+    $(selecor).html(toolbarHtml) ;
+    this.title = $("#toolbar-title") ;
+    this.backButton = $("#toolbar-back-button") ;
+    // callback(this);
+    // $(selecor).load("/TransactionSystem/commons/toolbar.html", function(){
+    //     this_toolbar.title = $("#toolbar-title") ;
+    //     this_toolbar.backButton = $("#toolbar-back-button") ;
+    //     callback(this_toolbar);
+    // }) ;
 }
